69c43c2ff0ec618988b1b5e5c3500d4cd8ed7738
MattuW4 <>
2024-01-05 16:21:09 +0000

    Add prev sate function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,13 @@ import css from './App.module.css';
 // import Sidebar from "./components/Sidebar";
 import StatefulGreeting from "./components/StatefulGreeting";
 import StatefulGreetingWithCallback from "./components/StatefulGreetingWithCallback";
+import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState"; 
 
 function App() {
   return (
     <div className={css.App}>
       {/* <Sidebar /> */}
-      <StatefulGreetingWithCallback />
+      <StatefulGreetingWithPrevState />
     </div>
   );
 }
